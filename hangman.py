f1c75448dde38b5a7c1d5947c43da1b67ce1ec9c
Mieszko Kuzmierz <mieszko.kuzmierz@gmail.com>
2024-04-14 19:08:45 +0200

    Commit to save changes for today

--- hangman.py
+++ hangman.py
@@ -1,2 +1,33 @@
-# Hey! Welcome to my first project on GitHub! My name is Mieszko, I'm 37 years old, living in Poland. I used to learn Python and now I'm coming back to pursue new possibilities.
+import random
+print('\nHeeello!! Welcome to my HANGMAN game!\n')
+
+words = ['Cow', 'Chicken', 'Dog', 'Bird', 'Crocodile', 'Snake']
+word = random.choice(words)
+print(word)
+
+print('Let\'s begin! Your word to guess the word below in 10 attempts!: \n')
+attempts = 10
+
+for i in word:
+    print(i.replace(i, '_ '), end= ' ')
+print('\n')
+
+while attempts > 0:
+
+    answer = input('Come on and guess the first letter: ').upper()
+
+    if answer in word:
+        print('Great! Indeed ' + answer + ' is in here!') 
+    else:
+        print('Kupsztal... ' + '\'' + answer + '\'' ' is not correct. you have '+ str(attempts - 1)+ ' tries!')
+    attempts = attempts - 1
+
+#Ok I'm a beginner programmer so 
+
+# for i in word: # Now we make a for loop to iterate through randomly selected word so that user can see how many letters does the word have
+#     print(i.replace(i, '_ '), end = '  ')
+
+# print('\n')
+
+# attempts = 10
 
