11d4620feeb297285ceb3396b3e89f9d66e25c7b
Mieszko Kuzmierz <mieszko.kuzmierz@gmail.com>
2024-04-18 09:49:32 +0200

    Final tweaks and adjustments

--- hangman.py
+++ hangman.py
@@ -1,37 +1,43 @@
 import random
+
 print('\nHeeello!! Welcome to my HANGMAN game!\n')
 
 words = ['Cow', 'Chicken', 'Dog', 'Bird', 'Crocodile', 'Snake']
 word = random.choice(words).upper()
 
 print('Let\'s begin! Your word to guess the word below in 10 attempts!: \n')
+
 attempts = 10
-
-for i in word:
-    print(i.replace(i, '_ '), end= ' ')
-print('\n')
-
 guessed_letters = []
 
-while attempts > 0:
-    answer = input('Guess the letter!: ').upper()
-    guessed_letters.append(answer)
+display_word = ['_'] * len(word)
+print(' '.join(display_word) + '\n')
 
-    if answer in word:
-        print('Good job! ' + answer + ' is in here:')
-        for char in word:
-            if char in guessed_letters:
-                print(char, end = ' ')
-            else:
-                print('_ ', end = ' ')
+while attempts > 0:
+    guess = input('Guess the letter!: ').upper()
+    
+
+    if guess in guessed_letters:
+        print('You already guessed that letter! Try again.')
+        continue
+
+    guessed_letters.append(guess)
+
+
+    if guess in word:
+        print('Good job! ' + guess + ' is in here:')
+        for i in range(len(word)):
+            if word[i] == guess:
+                display_word[i] = guess
+        print(' '.join(display_word))
         print()
     else:
-        attempts = attempts - 1
-        print('Bah! ' + '\'' + answer + '\'' + ' is not correct. You have ' + str(attempts) + ' more tries!')
+        attempts -= 1
+        print('Bah! ' + '\'' + guess + '\'' + ' is not correct. You have ' + str(attempts) + ' more tries!')
 
-    if set(word) == set(guessed_letters):
+    if '_' not in display_word:
         print('Congrats! You have guessed the word: ', word)
         break
-if attempts == 0:
+else:
     print('Sorry! you did not manage to guess the word in 10 tries')
 
